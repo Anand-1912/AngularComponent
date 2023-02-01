e0c7603ada6cb4e33f58e7ca985b5d42ffa7997d
Anand-1912 <anandchandrasekaran1912@gmail.com>
2023-02-01 13:12:04 +0530

    add Bootstrap in the inline style

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-notification',
-  template: `<div>
+  template: `<div class="alert alert-success">
     <p>This website uses cookies to provide better user experience</p>
   </div>`,
   styles: [
-    'div{background-color: orange;margin: 10px 0px; padding: 10px 20px; text-align:center;}',
+    '.notification-div{background-color: #FAD7A0;margin: 10px 0px; padding: 10px 20px; text-align:center;}',
     'p{font-size: 14px;}',
   ],
 })
