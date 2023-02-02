ee7f163a518f5a1b23fb3f06c4533800cba0d771
Anand-1912 <anandchandrasekaran1912@gmail.com>
2023-02-02 13:58:21 +0530

    add ngIf directive to enable you search for text

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./search.component.css'],
 })
 export class SearchComponent {
-  searchValue: string = 'mobile';
+  searchValue: string = '';
   changeSearchValue(eventData: Event) {
     //console.log((<HTMLInputElement>eventData.target).value);
     this.searchValue = (<HTMLInputElement>eventData.target).value;
