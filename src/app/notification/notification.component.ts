76c6f66eaae74ef60c1ada2a640388596cd71d85
Anand-1912 <anandchandrasekaran1912@gmail.com>
2023-02-02 15:27:18 +0530

    fadeout notification template using ngClass

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-notification',
-  template: `<div class="alert alert-success" [hidden]="hideNotification">
+  template: `<div
+    class="alert alert-success"
+    [ngClass]="{ fadeOut: hideNotification }"
+  >
     This website uses cookies to provide better user experience
     <div class="close">
       <button class="btn" (click)="closeNotification()">X</button>
@@ -12,6 +15,7 @@ import { Component } from '@angular/core';
     'div{margin: 10px 0px; padding: 10px 20px; text-align:center;}',
     'p{font-size: 14px;}',
     '.close{float: right; margin-top: -15px;}',
+    '.fadeOut {visibility: hidden;opacity: 0; transition: visibility 0s 2s, opacity 2s linear;}',
   ],
 })
 export class NotificationComponent {
